91d760b5c2f81a20b742685d6a09302ce89f27a8
Daniel Schwartz <dschwartz88@gmail.com>
2022-09-01 14:12:24 -0400

    fixup: use our own polyfill instead of package (because of bundle size concerns)

--- src/polyfill.ts
+++ src/polyfill.ts
@@ -1,12 +1,8 @@
-/**
- * The types for the below package exist but are incorrect. They don't include the available `shim()` method on the main export which
- * is what you should be calling to actually polyfill the missing function
- */
-// eslint-disable-next-line @typescript-eslint/no-var-requires, @typescript-eslint/no-unsafe-assignment
-const fromEntries = require('object.fromentries')
-
-// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
 if (!Object.fromEntries) {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-  Object.fromEntries = fromEntries.shim()
+  Object.fromEntries = function fromEntries (iterable: any) {
+    return [...iterable].reduce((obj, [key, val]) => {
+      obj[key] = val
+      return obj
+    }, {})
+  }
 }
