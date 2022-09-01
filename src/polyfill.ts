0f2dfd9437358355c7b3a6fa3349914d031f6dd2
Daniel Schwartz <dschwartz88@gmail.com>
2022-09-01 14:20:13 -0400

    fixup: ts/eslint errors

--- src/polyfill.ts
+++ src/polyfill.ts
@@ -1,6 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
 if (!Object.fromEntries) {
-  Object.fromEntries = function fromEntries (iterable: any) {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  Object.fromEntries = function fromEntries(iterable: unknown[][]) {
     return [...iterable].reduce((obj, [key, val]) => {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
       obj[key] = val
       return obj
     }, {})
